6ee500aa097d47545ea6f483b10a52ff3973943a
nithyaganesh-dev <nithyaganesh.developer@gmail.com>
2025-06-11 12:03:31 +0530

    Implement basic express app

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+const app = express();
+
+// Middlewares 
+app.use(express.json());
+app.use(cors());
+
+// Routes 
+app.get("/", (req, res) => res.send("Blog API Working"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
