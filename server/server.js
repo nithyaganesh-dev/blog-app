286b124fd026778864976b8e8d1b38a86c3f4fe8
nithyaganesh-dev <nithyaganesh.developer@gmail.com>
2025-06-11 12:18:41 +0530

    Implement MongoDB database connectivity

--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
-import express from 'express';
-import 'dotenv/config';
-import cors from 'cors';
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
 
 const app = express();
 
-// Middlewares 
+await connectDB();
+
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
-// Routes 
+// Routes
 app.get("/", (req, res) => res.send("Blog API Working"));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 export default app;
